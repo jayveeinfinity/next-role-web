d0c6845604de88b014a938dba9e00ebcaab4e209
John Vincent Bonza <johnvincent.bonza@gmail.com>
2026-02-05 16:43:42 +0800

    Initialize Vue.js with Inertia.js

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import HomeLayout from '@Shared/Layouts/Home.vue';
+
+    defineOptions({
+        layout: HomeLayout
+    });
+</script>
+
+<template>
+    <main>
+        Test
+    </main>
+</template>
